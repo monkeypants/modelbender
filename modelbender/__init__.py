ae50a823017934915a15d3559d2004ebac65159d
Chris Gough <christopher.d.gough@gmail.com>
2017-11-06 22:25:29 +1100

    fix path/import problem

--- modelbender/__init__.py
+++ modelbender/__init__.py
@@ -1,1 +1,8 @@
+import os.path
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.insert(0, currentdir)
+
 import metamodel
+
