4aa0029b54f3fad516297104973586d4544752c4
Chris Gough <christopher.d.gough@gmail.com>
2018-04-10 19:23:33 +1000

    cleanup now that render command works
    re: #1

--- dev/build_examples.sh
+++ dev/build_examples.sh
@@ -4,14 +4,10 @@ cd ../examples/gosource
 # equate pwd to /work/, because indir/outdir are relative to that
 #docker run -v `pwd`:/work/ model-bender validate --indir=metamodel
 docker run -v `pwd`:/work/ model-bender enterprise --indir=metamodel
+docker run -v `pwd`:/work/ model-bender render
 
-# this is a horrible kluidge
-# FIXME: container generates files owned by root
-sudo chown chtis:chtis _tmp/*
-sudo chown chtis:chtis _tmp/*/*
-#sudo chown chtis:chtis _tmp/*/*/*
 
-../../scripts/mk_diagrams.sh
-cd _tmp
-./venv.sh make html
+# TODO: push this into another command, like render
+#cd _tmp
+#./venv.sh make html
 #make latexpdf
